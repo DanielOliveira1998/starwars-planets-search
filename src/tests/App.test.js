722d9a23091916c75aa8fde5995ef15e65cc5205
Daniel Oliveira <danoliveira865@gmail.com>
2023-02-14 00:53:31 -0300

    86% coverage.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,224 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import App from '../App';
+import DataProvider from '../context/DataPlanets';
+import apiData from './helper/apiData';
+import userEvent from '@testing-library/user-event';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testa tabela', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(apiData),
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('Valida renderização de elementos do componente da tabela,', async () => {
+    render(
+      <DataProvider>
+        <App />)
+      </DataProvider>
+    )
+    const searchInput = screen.getByTestId('name-filter');
+    const colunaTag = screen.getByText(/coluna/i);
+    const colunaSelect = screen.getByRole('combobox', {
+      name: /coluna/i
+    });
+    const operadorTag = screen.getByText(/operator/i);
+    const operadorSelect = screen.getByRole('combobox', {
+      name: /operator/i
+    });
+    const valueInput = screen.getByTestId('value-filter');
+    const filtroBtn = screen.getByRole('button', {
+      name: /filtro/i
+    });
+    const removeFiltersBtn = screen.getByRole('button', {
+      name: /remove filters/i
+    });
+    const tableTitle = screen.getAllByRole('columnheader');
+    const tableContent = await screen.findAllByRole('cell');
+    expect(searchInput).toBeInTheDocument();
+    expect(colunaTag).toBeInTheDocument();
+    expect(colunaSelect).toBeInTheDocument();
+    expect(operadorTag).toBeInTheDocument();
+    expect(operadorSelect).toBeInTheDocument();
+    expect(valueInput).toBeInTheDocument();
+    expect(filtroBtn).toBeInTheDocument();
+    expect(removeFiltersBtn).toBeInTheDocument();
+    expect(tableTitle.length).toBe(13);
+    expect(tableContent.length).toBe(130);
+  })
+
+  test('testa a função handleSearch', () => {
+    render(
+      <DataProvider>
+        <App />
+      </DataProvider>
+    );
+    const searchInput = screen.getByTestId('name-filter');
+    fireEvent.change(searchInput, { target: { value: 'Tatooine' } });
+    expect(searchInput.value).toBe('Tatooine');
+  });
+
+  test('testa a função handleOperator', () => {
+    render(
+      <DataProvider>
+        <App />
+      </DataProvider>
+    );
+    const operatorInput = screen.getByTestId('comparison-filter');
+    fireEvent.change(operatorInput, { target: { value: 'menor que' } });
+    expect(operatorInput.value).toBe('menor que');
+  });
+
+  test('testa a função handleNumberValue', () => {
+    render(
+      <DataProvider>
+        <App />
+      </DataProvider>
+    );
+    const numberValueInput = screen.getByTestId('value-filter');
+    fireEvent.change(numberValueInput, { target: { value: '100000' } });
+    expect(numberValueInput.value).toBe('100000');
+  });
+
+  test('Valida o funcionamento do botão de filtro', async () => {
+    render(
+      <DataProvider>
+        <App />)
+      </DataProvider>
+    )
+    const colunaSelect = screen.getByRole('combobox', {
+      name: /coluna/i
+    });
+    const operadorSelect = screen.getByRole('combobox', {
+      name: /operator/i
+    });
+    const valueInput = screen.getByTestId('value-filter');
+    const filtroBtn = screen.getByRole('button', {
+      name: /filtro/i
+    });
+    userEvent.selectOptions(colunaSelect, ['population']);
+    userEvent.selectOptions(operadorSelect, ['maior que']);
+    userEvent.type(valueInput, 0);
+    userEvent.click(filtroBtn);
+    waitFor(() => {
+      const tableContent = screen.getAllByRole('cell');
+      expect(tableContent.length).toBe(104);
+      const removeBtn = screen.getByRole('button', {
+        name: /x/i
+      })
+      userEvent.click(removeBtn)
+    })
+  })
+
+  test('Valida o funcionamento do botão remove filters', () => {
+    render(
+      <DataProvider>
+        <App />)
+      </DataProvider>
+    )
+    const colunaSelect = screen.getByRole('combobox', {
+      name: /coluna/i
+    });
+    const operadorSelect = screen.getByRole('combobox', {
+      name: /operator/i
+    });
+    const valueInput = screen.getByTestId('value-filter');
+    const filtroBtn = screen.getByRole('button', {
+      name: /filtro/i
+    });
+    const removeFilters = screen.getByRole('button', {
+      name: /remove filters/i
+    });
+    userEvent.selectOptions(colunaSelect, ['population']);
+    userEvent.selectOptions(operadorSelect, ['maior que']);
+    userEvent.type(valueInput, 0);
+    userEvent.click(filtroBtn);
+    userEvent.selectOptions(colunaSelect, ['rotation_period']);
+    userEvent.selectOptions(operadorSelect, ['igual a']);
+    userEvent.type(valueInput, 24);
+    userEvent.click(filtroBtn);
+    userEvent.click(removeFilters)
+  })
+
+  test('Valida filtro menor que', async () => {
+    render(
+      <DataProvider>
+        <App />)
+      </DataProvider>
+    )
+    const tableContent = await screen.findAllByRole('cell');
+    expect(tableContent.length).toBe(130);
+    const colunaSelect = screen.getByRole('combobox', {
+      name: /coluna/i
+    });
+    const operadorSelect = screen.getByRole('combobox', {
+      name: /operator/i
+    });
+    const valueInput = screen.getByTestId('value-filter');
+    userEvent.selectOptions(colunaSelect, ['population']);
+    userEvent.selectOptions(operadorSelect, ['maior que']);
+    userEvent.type(valueInput, 1000000);
+    const filtroBtn = screen.getByRole('button', {
+      name: /filtro/i
+    })
+    userEvent.click(filtroBtn);
+    await waitFor(() => {
+      const tableContent2 = screen.getAllByRole('cell');
+      expect(tableContent2.length).toBe(104);
+    })
+  })
+
+  test('Valida filtro menor que', async () => {
+    render(
+      <DataProvider>
+        <App />)
+      </DataProvider>
+    )
+    const tableContent = await screen.findAllByRole('cell');
+    expect(tableContent.length).toBe(130);
+    const colunaSelect = screen.getByRole('combobox', {
+      name: /coluna/i
+    });
+    const operadorSelect = screen.getByRole('combobox', {
+      name: /operator/i
+    });
+    const valueInput = screen.getByTestId('value-filter');
+    userEvent.selectOptions(colunaSelect, ['diameter']);
+    userEvent.selectOptions(operadorSelect, ['menor que']);
+    userEvent.type(valueInput, 5000);
+    const filtroBtn = screen.getByRole('button', {
+      name: /filtro/i
+    })
+    userEvent.click(filtroBtn);
+  })
+
+  test('Valida filtro igual a', async () => {
+    render(
+      <DataProvider>
+        <App />)
+      </DataProvider>
+    )
+    const tableContent = await screen.findAllByRole('cell');
+    expect(tableContent.length).toBe(130);
+    const colunaSelect = screen.getByRole('combobox', {
+      name: /coluna/i
+    });
+    const operadorSelect = screen.getByRole('combobox', {
+      name: /operator/i
+    });
+    const valueInput = screen.getByTestId('value-filter');
+    userEvent.selectOptions(colunaSelect, ['diameter']);
+    userEvent.selectOptions(operadorSelect, ['igual a']);
+    userEvent.type(valueInput, 5000);
+    const filtroBtn = screen.getByRole('button', {
+      name: /filtro/i
+    })
+    userEvent.click(filtroBtn);
+  })
+})
